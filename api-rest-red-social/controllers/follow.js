11cd380813052d29fd5506cb9459cc7c373040a0
MiltonPonceRodriguez1 <miltonponceipn@gmail.com>
2023-01-30 19:11:34 -0600

    model-and-schema-follow

--- api-rest-red-social/controllers/follow.js
+++ api-rest-red-social/controllers/follow.js
@@ -1,11 +1,29 @@
-// ACCIONES DE PRUEBA
+// IMPORTACION DE MODELOS
+const Follow = require('../models/follow');
+const User = require('../models/user');
 
+// ACCIONES DE PRUEBA
 const followTest = (req, res) => {
     return res.status(200).send({
         message: "Mensaje enviado desde: controllers/follow.js"
     });
 }
 
+// ACCIÓN DE SEGUIR (GUARDAR FOLLOW)
+const save = (req, res) => {
+    return res.status(200).json({
+        status: 'success',
+        message: 'Metodo de follow'
+    })
+}
+
+// ACCIÓN DEJAR DE SEGUIR (ELIMINAR FOLLOW)
+
+// ACCIÓN DE LISTA DE USUARIOS QUE ESTOY SIGUIENDO
+
+// ACCIÓN DE LISTA DE USUARIOS QUE ME SIGUEN
+
 module.exports = {
-    followTest
+    followTest,
+    save
 }
